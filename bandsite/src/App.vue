c41d777a71c58fbc2f0e281e98866bead16fdf9e
Philippe Recto <precto1285@gmal.com>
2018-12-26 14:31:17 -0800

    added footer content

--- bandsite/src/App.vue
+++ bandsite/src/App.vue
@@ -1,16 +1,20 @@
 <template>
   <div id="app">
     <nav-bar></nav-bar>
+
+    <footer-contact></footer-contact>
   </div>
 </template>
 
 <script>
 import NavBar from "./components/NavBar.vue";
+import FooterContact from "./components/Footer.vue";
 
 export default {
   name: "app",
   components: {
-    NavBar
+    NavBar,
+    FooterContact
   }
 };
 </script>
