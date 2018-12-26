9e44a6bb6f50618773f9552a6dc235508f91d424
Philippe Recto <precto1285@gmal.com>
2018-12-26 14:24:34 -0800

    added navbar

--- bandsite/src/App.vue
+++ bandsite/src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    <hello-world></hello-world>
+    <nav-bar></nav-bar>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import NavBar from "./components/NavBar.vue";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    NavBar
   }
 };
 </script>
